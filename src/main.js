35f56d7b59ebb84ff571cd25a2bb027be3ef6995
Farid Sabitov <farid.sabitov.design@gmail.com>
2019-10-05 10:51:25 +0300

    adding update name functionality

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ let symbols = selection.layers.filter(layer => layer.type == sketch.Types.Symbol
 let symbol = null;
 
 if(selection.length == 1 && selection.layers[0].type == "SymbolMaster"){
-  symbol = selection.layers[0];
+  symbol = selection.layers[0];  
 } else {
   UI.message('Please select the symbol on the "Symbols" page to manage overrides 🤓')
 }
@@ -21,6 +21,7 @@ if(selection.length == 1 && selection.layers[0].type == "SymbolMaster"){
 export function enableAll(){
   if(symbol){
     for(let g = 0; g < symbol.overrides.length; g++) {
+      
       symbol.overrides[g].editable = true
     }
   }
@@ -29,12 +30,37 @@ export function enableAll(){
 export function enableDynamic(){
   if(symbol){
     for(let g = 0; g < symbol.overrides.length; g++) {
-      log(symbol.overrides[g].value)
-      if (symbol.overrides[g].value.includes("{") && symbol.overrides[g].value.includes("}")) {
+      // log(symbol.overrides[g].property)
+      if (symbol.overrides[g].sketchObject.affectedLayer().name().includes("{") && symbol.overrides[g].sketchObject.affectedLayer().name().includes("{")) {
         symbol.overrides[g].editable = true;
       } else {
         symbol.overrides[g].editable = false;
       }
+      if(symbol.overrides[g].property == "stringValue"){
+        log(symbol.overrides[g].sketchObject.affectedLayer().id())
+      }
+      // if(symbol.overrides[g].property == "stringValue"){
+      //   log(symbol.overrides[g].sketchObject.affectedLayer().name())
+      //   if (symbol.overrides[g].value.includes("{") && symbol.overrides[g].value.includes("}")) {
+      //     symbol.overrides[g].editable = true;
+      //   } else {
+      //     symbol.overrides[g].editable = false;
+      //   }
+      // }
+      // if(symbol.overrides[g].property == "image"){
+      //   if (symbol.overrides[g].sketchObject.affectedLayer().name().includes("{") && symbol.overrides[g].sketchObject.affectedLayer().name().includes("{")) {
+      //     symbol.overrides[g].editable = true;
+      //   } else {
+      //     symbol.overrides[g].editable = false;
+      //   }
+      // }
+      // if(symbol.overrides[g].property == "symbolID"){
+      //   if (symbol.overrides[g].sketchObject.affectedLayer().name().includes("{") && symbol.overrides[g].sketchObject.affectedLayer().name().includes("{")) {
+      //     symbol.overrides[g].editable = true;
+      //   } else {
+      //     symbol.overrides[g].editable = false;
+      //   }
+      // }
     }
   }
 }
@@ -45,6 +71,14 @@ export function disableAll(){
       symbol.overrides[g].editable = false
     }
   }
+}
+
+export function updateName(){
+  // if(symbol){
+  //   for(let g = 0; g < symbol.overrides.length; g++) {
+  //     symbol.overrides[g].editable = false
+  //   }
+  // }
 }
 
 export function howWorks(){
